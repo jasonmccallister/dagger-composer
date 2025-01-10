8a69c5a14354b9ef6476c2645327a0598fd85d9f
Jason McCallister <jason@dagger.io>
2025-01-10 00:19:36 -0500

    docs

--- main.go
+++ main.go
@@ -1,16 +1,8 @@
-// A generated module for Composer functions
+// Dagger Composer Module
 //
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
+// This module provides a way to install composer dependencies in a directory for a
+// PHP project. It uses the composer image to install the dependencies and returns the
+// vendor directory with the dependencies installed.
 
 package main
 
